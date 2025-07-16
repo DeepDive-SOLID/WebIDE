c54987917fe16047bba17c39645a62bc69a75ffd
lnylnylnylny <lnylnylnylny@naver.com>
2025-07-17 02:07:39 +0900

    feat: 현재 컨테이너 정보 추가

--- frontend/src/components/APP/AppSidebar.tsx
+++ frontend/src/components/APP/AppSidebar.tsx
@@ -1,9 +1,10 @@
 import "../../styles/AppSidebar.scss";
 import Box from "../UI/box";
+import Bargraph from "../UI/Bargraph";
 import { LuBox } from "react-icons/lu";
-
 import { IoListOutline } from "react-icons/io5";
 import { CiFileOn } from "react-icons/ci";
+import { FaUsers } from "react-icons/fa";
 
 export interface AppSidebarProps {
   isOpen: boolean;
@@ -37,22 +38,43 @@ const AppSidebar = ({ isOpen, type }: AppSidebarProps) => {
       )}
 
       {type === "algorithm" && (
-        <div className="section">
-          <h2>Algorithm</h2>
-          <div className="box-list">
-            <Box
-              icon={<IoListOutline size={20} />}
-              title="기초수학"
-              onClick={() => {}}
-              className="box-primary"
-            />
-            <Box
-              icon={<CiFileOn size={20} />}
-              title="더하기"
-              onClick={() => {}}
-            />
+        <>
+          <div className="section top-section">
+            <h2>Algorithm</h2>
+            <div className="box-list">
+              <Box
+                icon={<IoListOutline size={20} />}
+                title="기초수학"
+                onClick={() => {}}
+                className="box-primary"
+              />
+              <Box
+                icon={<CiFileOn size={20} />}
+                title="더하기"
+                onClick={() => {}}
+              />
+            </div>
           </div>
-        </div>
+
+          <div className="section bottom-section">
+            <div className="team-status">
+              <h3>팀원 현황(3 /5)</h3>
+              <Bargraph name="user1" language="JS" success={4} total={4} />
+              <Bargraph name="user2" language="JS" success={3} total={4} />
+              <Bargraph name="user3" language="JS" success={2} total={4} />
+              <Bargraph name="" language="" success={0} total={4} />
+              <Bargraph name="" language="" success={0} total={4} />
+            </div>
+
+            <div className="current-container">
+              <FaUsers className="container-icon" />
+              <div className="container-texts">
+                <p className="label">현재 컨테이너</p>
+                <p className="name">SOLID 컨테이너</p>
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
