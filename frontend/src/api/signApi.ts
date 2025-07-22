d32af0d2235716afd2f65e7dd0b385e9510ff474
dyoni2 <delay1820@gachon.ac.kr>
2025-07-22 11:05:18 +0900

    chore: axios 라이브러리 추가

--- frontend/src/api/signApi.ts
+++ frontend/src/api/signApi.ts
@@ -32,7 +32,7 @@ export const signApi = {
   checkId: async (signInCheckIdDto: SignInCheckIdDto): Promise<boolean> => {
     try {
       const res = await signAxios.post("/sign/checkId", signInCheckIdDto);
-      return res.data; // true면 아이디가 있음, false면 아이디가 없음
+      return res.data as boolean; // true면 아이디가 있음, false면 아이디가 없음
     } catch {
       throw new Error("ID 중복확인 중 오류가 발생했습니다.");
     }
@@ -44,7 +44,7 @@ export const signApi = {
   ): Promise<boolean> => {
     try {
       const res = await signAxios.post("/sign/checkEmail", signInCheckEmailDto);
-      return res.data; // true면 이메일이 있음, false면 이메일이 없음
+      return res.data as boolean; // true면 이메일이 있음, false면 이메일이 없음
     } catch {
       throw new Error("이메일 중복확인 중 오류가 발생했습니다.");
     }
@@ -53,13 +53,13 @@ export const signApi = {
   // 로그인
   login: async (signInDto: SignInDto): Promise<string> => {
     const res = await signAxios.post("/sign/login", signInDto);
-    return res.data; // JWT 문자열 반환
+    return res.data as string; // JWT 문자열 반환
   },
 
   // 아이디 찾기
   findId: async (signFindIdDto: SignFindIdDto): Promise<string> => {
     const res = await signAxios.post("/sign/findId", signFindIdDto);
-    return res.data; // memberId 반환
+    return res.data as string; // memberId 반환
   },
 
   // 비밀번호 찾기 - 인증
@@ -67,7 +67,7 @@ export const signApi = {
     signCheckIdEmailDto: SignCheckIdEmailDto
   ): Promise<{ success: boolean; message?: string }> => {
     const res = await signAxios.post("/sign/checkIdEmail", signCheckIdEmailDto);
-    return res.data;
+    return res.data as { success: boolean; message?: string };
   },
 
   // 비밀번호 재설정
@@ -75,12 +75,12 @@ export const signApi = {
     signUpdPwDto: SignUpdPwDto
   ): Promise<{ success: boolean; message?: string }> => {
     const res = await signAxios.put("/sign/updPw", signUpdPwDto);
-    return res.data;
+    return res.data as { success: boolean; message?: string };
   },
 
   // 토큰 재발급
   refreshToken: async (): Promise<string> => {
     const res = await signAxios.post("/token/refresh");
-    return res.data; // 새로운 access token 반환
+    return res.data as string; // 새로운 access token 반환
   },
 };
