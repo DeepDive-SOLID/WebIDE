8e58cba113da0d43b5d952f0d5c0f6c498bad933
lnylnylnylny <lnylnylnylny@naver.com>
2025-07-22 11:01:27 +0900

    fix: 색상 헥스값으로 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @use "./../styles/variables" as *;
-        `,
-      },
-    },
-  },
 });
