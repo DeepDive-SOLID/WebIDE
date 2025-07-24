46428a8f34f2a4a343a02d269363f52516d9eafb
lnylnylnylny <lnylnylnylny@naver.com>
2025-07-25 01:04:37 +0900

    feat: 파일추가 모달창 추가

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,17 @@
 import AlgorithmNav from "../components/APP/Nav/AlgorithmNav";
+import AddFileModal from "../components/APP/AddFileModal";
+import { useState } from "react";
 
 const Home = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <div>
-      <AlgorithmNav />
+      <button onClick={openModal}>모달창 열기</button>
+      {isModalOpen && <AddFileModal onClose={closeModal} />}
     </div>
   );
 };
