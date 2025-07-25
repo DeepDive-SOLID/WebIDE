9e16ce60216680e58bc46e4c1d07efefcc45dad8
dyoni2 <delay1820@gachon.ac.kr>
2025-07-25 16:53:44 +0900

    chore: 파일명 변경

--- frontend/src/components/UI/Modal.tsx
+++ frontend/src/components/UI/Modal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../styles/Modal.module.scss";
+import styles from "../../styles/AppModal.module.scss";
 
 interface ModalProps {
   children: React.ReactNode;
