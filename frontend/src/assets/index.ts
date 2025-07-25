64ec7d563136594066bb8aec6d8ff3c3bb98aed3
lnylnylnylny <lnylnylnylny@naver.com>
2025-07-26 04:23:35 +0900

    refactor: 이미지 파일 변경 및 파일 정리

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,4 +1,9 @@
-export { default as profile } from "./images/profile.svg";
+// 아이콘
 export { default as tab } from "./icons/tab.svg";
 export { default as enter } from "./icons/enter.svg";
 export { default as spacebar } from "./icons/spacebar.svg";
+export { default as backIcon } from "./icons/back.svg";
+
+// 이미지
+export { default as logoImg } from "./images/CodeWith.svg";
+export { default as profile } from "./images/profile.svg";
