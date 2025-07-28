ef03bca6ff0ba61633c195c44cb404dc6cd38f30
w-jins <jineen@naver.com>
2025-07-28 17:22:36 +0900

    refactor: 디렉토리명 변경

--- frontend/src/pages/containerIde/Ide.tsx
+++ frontend/src/pages/containerIde/Ide.tsx
@@ -3,12 +3,13 @@ import Question from "../../components/ide/question/Question";
 import Header from "../../components/ide/header/Header";
 import Container from "../../components/ide/container/Container";
 import type { question } from "../../types/ide";
+import axios from "axios";
 
 const Ide = () => {
   // 로그인한 유저의 id 가져오기
   const [loginId, setLoginId] = useState<string | null>("");
   const [activeButtonId, setActiveButtonId] = useState<string | null>("");
-  const [question, setQuestion] = useState<question>({});
+  const [question, setQuestion] = useState<question>();
 
   // 클릭된 버튼의 ID를 인자로 받습니다.
   const handleOnClick = (id: string) => {
@@ -23,19 +24,13 @@ const Ide = () => {
     setActiveButtonId(id);
 
     const fetchQuestionData = async () => {
-      // 가상의 질문 데이터
-      const dummyQuestion: question = {
-        questionId: 1,
-        questionTitle: "더하기",
-        questionDescroption: "두 수를 더하는 함수를 작성하세요.",
-        question: "",
-        questionInput: "첫째 줄에 입력을 받은 값을 더하고 두번째 줄에 입력을 받아 더한다",
-        questionOutput: "더한 값만 나오고 출력한다.",
-        questionTime: 1,
-        questionMem: 1,
-        // 필요한 다른 필드 추가
-      };
-      setQuestion(dummyQuestion);
+      try {
+        const res = await axios.post("http://localhost:8080/question/list_id", { containerId: 1 });
+        console.log(res.data);
+        setQuestion(res.data);
+      } catch (e) {
+        console.error("에러: " + e);
+      }
     };
     fetchQuestionData();
   }, []);
