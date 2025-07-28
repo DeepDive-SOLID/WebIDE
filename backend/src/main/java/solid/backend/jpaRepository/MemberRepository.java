0f4a51d937da438872441c2056294205a8fbe465
Your Name <your-email@example.com>
2025-07-28 23:21:06 +0900

    feat: Sign 모듈 및 설정 파일 동기화

--- backend/src/main/java/solid/backend/jpaRepository/MemberRepository.java
+++ backend/src/main/java/solid/backend/jpaRepository/MemberRepository.java
@@ -3,5 +3,12 @@ package solid.backend.jpaRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import solid.backend.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, String> {
+    /**
+     * 설명: 아이디 찾기 이메일 확인
+     * @param email
+     */
+    Optional<Member> findByMemberEmail(String email);
 }
