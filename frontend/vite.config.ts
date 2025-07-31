2d9f58b0a6651b51e8cea137d32c2f1a0ce787af
dyoni2 <delay1820@gachon.ac.kr>
2025-07-31 11:11:59 +0900

    refactor: 들여쓰기 통일

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,25 +2,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            "/sign": {
-                target: "http://localhost:8080",
-                changeOrigin: true,
-            },
-            "/api": {
-                target: "http://localhost:8080",
-                changeOrigin: true,
-            },
-            "/token": {
-                target: "http://localhost:8080",
-                changeOrigin: true,
-            },
-            "/mypage": {
-                target: "http://localhost:8080",
-                changeOrigin: true,
-            },
-        },
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/sign": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/token": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/mypage": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
     },
+  },
 });
